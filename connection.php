2de1a96ff1505b5e7361deb1e9922ffa95dce728
xXyousefXx6768 <142452295+xXyousefXx6768@users.noreply.github.com>
2023-12-17 02:44:10 +0200

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+$username = "root";
+
+$localhost = "localhost";
+
+$pass ='';
+
+$dbname ='yousef_db';
+
+$conn=mysqli_connect($localhost,$username,$pass, $dbname);
+
+ if($conn){
+    echo'done ';
+
+
+ }else{
+
+   echo 'not done ';
+ }
+    
+
+
+
+
+?>
